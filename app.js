641295cd6b84ecad78e9674360a06bb48078ea82
Evan Harris <echarris@smcm.edu>
2017-06-19 21:09:17 -0400

    Changes state.page to state.view

--- app.js
+++ app.js
@@ -5,15 +5,10 @@
 // Create your initial state object
 var appState = {
     questions: [],
+    currentQuestion: 0,
     score: 0,
     correctAnswer: [],
-    page: 0
-
-    //page 0: start; page 1: question; page 2: results; 3: final;
-    // User's answer choice(s)	
-    // Has quiz started?
-    // What is the current question?
-    // Other things like score? Anything else?
+    view: 'question'
 };
 
 
@@ -52,50 +47,8 @@ var appState = {
 //   let i
 //     let random = (Math.floor(Math.Random()*10);
 
-  
 // }
 
-//for Current ?: index+1 edge case for question 5: may produce undefined
-let questionTemplate = (state, index) => {
-  return (`<h2>Question #: ${state.questions[index].question}</h2>
-      <form>
-        <fieldset>
-          <legend> Question: </legend>
-          <input id="answer0" type="radio" name="answer" value="0" required>	
-          <label for="answer0">${state.questions[index].choice[0]}</label>
-          <input id="answer0" type="radio" name="answer" value="1">
-          <label for="answer1">${state.questions[index].choice[1]}	</label>				
-          <input for="answer0" type="radio" name="answer" value="2">
-          <label id="answer2">${state.questions[index].choice[2]}</label>					
-          <input for="answer0" type="radio" name="answer" value="3">
-          <label id="answer3">${state.questions[index].choice[3]}</label>			
-        </fieldset>
-        <button type="submit jq-question-submit">Submit Answer</button>
-		  </form>
-      <p class="js-current-question">Current ?: ${state.questions[index+1]} out of 5</p>
-      <p class="js-score">Score: ${state.score}</p>`);
-  };
-
-
-let correctAnswerTemplate = (state, index) => {
-  return (`
-    <div class="js-feedback-right">
-      <h3>You were right!</h3>
-      <p>Press next to proceed to next question:</p>
-      <button type="submit">Next Question</button>
-    </div>
-  `);
-};
-
-let wrongAnswerTemplate = (state, index) => {
-  return (`
-    <div class="js-feedback-wrong">
-      <h3>Oops, you got that one wrong.</h3>
-      <p>Press next to proceed to next question:</p>
-      <button type="submit">Next Question</button>
-    </div>
-  `);
-};
 // State manipulation functions
 
 function isUserInputCorrect() {
@@ -106,13 +59,18 @@ function randomQuestion(state) {
 
 }
 
-function startQuiz(state) {
+function updateViewToQuestionPage(state, view) {
 	//move from start page to the questions page
 	//move from 0 to 1
-	appState.page++;
-	renderPage(state, appState.page, $('body'))
+  state.view = view;
+	renderPage(state, $('body'));
 }
 
+function updateViewToFeedbackPage(state, view) {
+  //move from question page to the feedback page
+  state.view = 'feedback';
+  renderPage(state, $('body'));
+}
 // get question
 
 
@@ -138,48 +96,106 @@ const resetQuiz = function(state) {
 }
 
 // Render functions...
-function renderQuestion(state,index,element) {
-  console.log(`I am on question: ${index}`);
+function renderQuestion(state, element) {
+  console.log(`I am on question: ${state.currentQuestion}`);
 
   $('.js-questions-page').removeClass('hidden');
 
   const myHTML = element.find('.js-questions-page');
-  $(myHTML).html(questionTemplate(state, index));
+  $(myHTML).html(questionTemplate(state, state.currentQuestion));
 }
 
-function renderResults(state,index,element) {
-  console.log(`I am looking at results after question: ${index}`);
+//This function renders the feedback page (if the button worked)
+function renderFeedback(state, element) {
+  console.log(`I am looking at results after question: ${state.currentQuestion}`);
+
+  $('.js-questions-page').addClass('hidden');
+  $('.js-feedback-page').removeClass('hidden');
+
+  const myHTML = element.find('.js-feedback-page');
+  $(myHTML).html(feedbackTemplate(state, state.currentQuestion));
 }
 
-// X pages:
-  //0: start
-  //1: question (1)
-  //2: Results
-  //3: questions (2)
-  //4: results
-  //5: questions (3)
-  //6: results
-  //7: questions (4)
-  //8: results
-  //9: questions (5)
-  //10: Final Page
+  //Render Templates:
+let questionTemplate = (state, index) => {
+  return (`
+    <h2>Question: ${state.questions[index].question}</h2>
+      <form class="js-form">
+        <fieldset>
+          <legend> Question: </legend>
+          <input id="answer0" type="radio" name="answer" value="0" required>  
+          <label for="answer0">${state.questions[index].choice[0]}</label>
+          <input id="answer0" type="radio" name="answer" value="1">
+          <label for="answer1">${state.questions[index].choice[1]}  </label>        
+          <input for="answer0" type="radio" name="answer" value="2">
+          <label id="answer2">${state.questions[index].choice[2]}</label>         
+          <input for="answer0" type="radio" name="answer" value="3">
+          <label id="answer3">${state.questions[index].choice[3]}</label>     
+        </fieldset>
+        <button type="submit" class="js-question-submit">Submit Answer</button>
+      </form>
+      <p class="js-current-question">Current Question: ${state.currentQuestion} out of 5</p>
+      <p class="js-score">Score: ${state.score}</p>
+      `);
+  };
 
-function renderPage(state, currentPage, element) {
-	//Start visible; 3 hidden
-	//Questions page visible; 3 hidden
 
-     if (currentPage === 0 ) {
+let feedbackTemplate = (state, index) => {
+  //todo : create ternary / if what outputs return You were right or You were wrong depending on answer
+  return (`
+      <h3>You were right!</h3>
+      <p>Press next to proceed to next question:</p>
+      <button type="submit">Next Question</button>
+  `);
+};
+
+function renderPage(state, element) {
+  //Start visible; 3 hidden
+  $('div[class$="-page"').addClass('hidden');
+
+     if (state.view === 'start' ) {
       //startPageRenderFunction():
-     } else if (currentPage === 1) {
-      $('.js-start-page').addClass('hidden')
-      renderQuestion(state, 0, $('body'));
-      // update score
-    } else if (currentPage === 2) {
-      $('.js-questions-page').addClass('hidden')
-      $('.js-feedback').addClass('hidden')
-    } 
+     } else if (state.view === 'question') {
+        console.log('we in here');
+        renderQuestion(state, $('body'));
+        $('.js-questions-page').removeClass('hidden');
+        // update score
+    } else if (state.view === 'feedback') {
+        console.log('derp')
+        renderFeedback(state, $('body'))
+        $('.js-feedback-page')
+    } else if (state.view === 'final') {
+
+    }
    
 }
+
+
+// Event handlers
+// When start button is submitted
+function handlesStart () {
+	$('.start').on('click', function(event) {
+  // 1. Update page view to question page
+  	updateViewToQuestionPage(appState,'question');
+	});
+}
+
+//Handleanswersubmit does not detect button clicks
+function handlesAnswerSubmit() {
+    $('.js-question-submit').submit(function(event) {
+    // 1. Retrieve from DOM - which answer was clicked?
+    let userChoice =  $(this).closest('input[type=radio]');
+    console.log(userChoice);
+    // 2. Change state view to feedback page
+  updateViewToFeedbackPage(appState,'feedback');
+  });
+}
+
+$(function() {
+	handlesStart();
+  handlesAnswerSubmit();
+  //When will currentQuestion be incremented?
+})
 
 /*      
       // start
@@ -217,30 +233,3 @@ function renderPage(state, currentPage, element) {
       $('.js-feedback-right').addClass('hidden')
       $('.js-results').removeClass('hidden')
 */
-
-// Event handlers
-// When start button is submitted
-function handlesStart () {
-	$('.start').on('click', function(event) {
-  // 1. Change state with state mod functio
-  	startQuiz(appState);
-  // 2. Invoke render function
-	});
-}
-
-// Answer is submitted
-$('.js-question-submit').on('click', function(event) {
-  // 1. Retrieve from DOM - which answer was clicked?
-  let userChoice =  $(this).closest('input[type=radio]');
-  console.log(userChoice);
-  // 2. Change state with state mod function
-
-  // 3. Invoke render function
-});
-
-$(function() {
-  renderPage(appState, appState.page, $('body'));
-	handlesStart();
-})
-
-//state.questions[index].choice[1]
