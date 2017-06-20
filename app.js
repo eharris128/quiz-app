88e1160c1b95b4a1edfa5e0ceef3c03489212981
dankato <apearmor@gmail.com>
2017-06-20 12:09:16 -0700

    updated css and rendering

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ var appState = {
         answer: 1,
       },
       {
-        question:'What is 10^3?', 
+        question:'What is cube root of 10,000?', 
         choice: ['300','10000','1000','100'], 
         answer: 2,
       },
@@ -61,17 +61,17 @@ var appState = {
 
 //for Current ?: index+1 edge case for question 5: may produce undefined
 let questionTemplate = (state, index) => {
-  return (`<h2>Question # ${state.currentQuestion+1}: <br>${state.questions[index].question}</h2>
+  return (`<h3>Question # ${state.currentQuestion+1}:</h3> <h2>${state.questions[index].question}</h2>
       <form id="js-form">
         <fieldset>
           <legend> Pick one: </legend>
           <input id="answer0" type="radio" name="answer" value="0" required>  
           <label for="answer0">${state.questions[index].choice[0]}</label>
-          <input id="answer0" type="radio" name="answer" value="1">
-          <label for="answer1">${state.questions[index].choice[1]}  </label>        
-          <input for="answer0" type="radio" name="answer" value="2">
+          <input id="answer1" type="radio" name="answer" value="1">
+          <label for="answer1">${state.questions[index].choice[1]}</label>        
+          <input for="answer2" type="radio" name="answer" value="2">
           <label id="answer2">${state.questions[index].choice[2]}</label>         
-          <input for="answer0" type="radio" name="answer" value="3">
+          <input for="answer3" type="radio" name="answer" value="3">
           <label id="answer3">${state.questions[index].choice[3]}</label>     
         </fieldset>
         <button class = "button" id="js-question-submit" type="submit">Submit Answer</button>
@@ -82,21 +82,17 @@ let questionTemplate = (state, index) => {
 
 let correctAnswerTemplate = (state, index) => {
   return (`
-    <div class="js-feedback-right-page">
       <h3>You were right!</h3>
-      <p>Press next to proceed to next question:</p>
-      <button type="submit">Next Question</button>
-    </div>
+      <p>Press "Next" to proceed to next question:</p>
+      <button class="button" type="submit">Next</button>
   `);
 };
 
 let wrongAnswerTemplate = (state, index) => {
   return (`
-    <div class="js-feedback-wrong-page">
       <h3>Oops, you got that one wrong.</h3>
-      <p>Press next to proceed to next question:</p>
-      <button type="submit">Next Question</button>
-    </div>
+      <p>Press "Next" to proceed to next question:</p>
+      <button class="button" type="submit">Next</button>
   `);
 };
 // State manipulation functions
@@ -130,6 +126,11 @@ function updateViewToFeedbackPage(state, view) {
   renderPage(state, $('body'));
 }
 
+function updateViewToResultsPage (state, view) {
+  state.view = view;
+  renderPage(state, $('body'));
+}
+
 // reset quiz
 const resetQuiz = function(state) {
   state.page = 0;
@@ -138,27 +139,30 @@ const resetQuiz = function(state) {
 
 // Render functions...
 function renderQuestion(state, element) {
-  console.log(`I am on question: ${state.currentQuestion}`);
+    console.log(`I am on question: ${state.currentQuestion}`);
 
-  $('.js-questions-page').removeClass('hidden');
+    $('.js-questions-page').removeClass('hidden');
 
-  const myHTML = element.find('.js-questions-page');
-  $(myHTML).html(questionTemplate(state, state.currentQuestion));
-}
+    const myHTML = element.find('.js-questions-page');
+    $(myHTML).html(questionTemplate(state, state.currentQuestion));
+  } 
+  
+
 
 function renderFeedback(state,element) {
-  console.log(`I am looking at results after question: ${state.currentQuestion}`);
+    console.log(`I am looking at results after question: ${state.currentQuestion}`);
 
-  $('.js-feedback-page').removeClass('hidden');
+    $('.js-feedback-page').removeClass('hidden');
 
-  const myHTML = element.find('.js-feedback-page');
+    const myHTML = element.find('.js-feedback-page');
 
-  if(state.lastQuestionCorrect) {
-    $(myHTML).html(correctAnswerTemplate(state, state.currentQuestion));
-  } else {
-    $(myHTML).html(wrongAnswerTemplate(state, state.currentQuestion));
-  }
+    if(state.lastQuestionCorrect) {
+      $(myHTML).html(correctAnswerTemplate(state, state.currentQuestion));
+    } else {
+      $(myHTML).html(wrongAnswerTemplate(state, state.currentQuestion));
+    }
 }
+
 
 function renderPage(state, element) {
   //Start visible; 3 hidden
@@ -172,8 +176,8 @@ function renderPage(state, element) {
     } else if (state.view === 'feedback') {
         renderFeedback(state, $('body'))
         $('.js-feedback-page').removeClass('hidden');
-    } else if (state.view === 'results') {
-
+    } else {
+      $('.js-results-page').removeClass('hidden');
     }
    
 }
@@ -183,7 +187,7 @@ function renderPage(state, element) {
     // 1. Retrieve from DOM - which answer was clicked?
     event.preventDefault();
     let userChoice = $(this).find("form input[name=answer]:checked").val();
-    console.log(userChoice);
+    console.log("user selected choice: " + userChoice);
     // 2. Change state with state mod function
     isUserInputCorrect(appState, userChoice);
   });
@@ -199,6 +203,16 @@ function handlesStart () {
 }
 
 // Answer is submitted
+$('.js-feedback-page').on("click", "button", function(event) {
+  event.preventDefault();
+  console.log('are you listening?')
+  appState.currentQuestion++;
+  if (appState.currentQuestion > 4) {
+    updateViewToResultsPage(appState,'results');
+  } else {
+    updateViewToQuestionPage(appState,'question');
+  }
+});
 
 
 $(function() {
@@ -213,36 +227,3 @@ document.getElementById("button").onclick = (function() {
     document.getElementsByTagName('audio')[0].play();
     return false;
 });
-
-/*      
-      // start
-      $('.js-start-page').removeClass('hidden')
-      $('.js-questions-page').addClass('hidden')
-      $('.js-feedback-wrong').addClass('hidden')
-      $('.js-feedback-right').addClass('hidden')
-      $('.js-results').addClass('hidden')
-      // questions
-      $('.js-start-page').addClass('hidden')
-      $('.js-questions-page').removeClass('hidden')
-      $('.js-feedback-wrong').addClass('hidden')
-      $('.js-feedback-right').addClass('hidden')
-      $('.js-results').addClass('hidden')
-      // feedback right
-      $('.js-start-page').addClass('hidden')
-      $('.js-questions-page').removeClass('hidden')
-      $('.js-feedback-wrong').addClass('hidden')
-      $('.js-feedback-right').addClass('hidden')
-      $('.js-results').addClass('hidden')
-      // feedback wrong
-      $('.js-start-page').addClass('hidden')
-      $('.js-questions-page').addClass('hidden')
-      $('.js-feedback-wrong').removeClass('hidden')
-      $('.js-feedback-right').addClass('hidden')
-      $('.js-results').addClass('hidden')
-      // results
-      $('.js-start-page').addClass('hidden')
-      $('.js-questions-page').addClass('hidden')
-      $('.js-feedback-wrong').addClass('hidden')
-      $('.js-feedback-right').addClass('hidden')
-      $('.js-results').removeClass('hidden')
-*/
